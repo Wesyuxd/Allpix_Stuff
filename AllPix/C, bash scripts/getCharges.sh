7454b5d7063ef1bfc3f3bf0a31c38ae930f8dac3
sofiya5050 <117371535+sofiya5050@users.noreply.github.com>
2024-07-12 10:10:11 -0400

    Update getCharges.sh

--- AllPix/C, bash scripts/getCharges.sh
+++ AllPix/C, bash scripts/getCharges.sh
@@ -10,10 +10,10 @@ echo "Data file name: "
 read DATAFILE
 CERNBOX="/eos/user/c/cmahajan/lxplus/allpix"
 ROOTFILE="$CERNBOX/$RUNFOLDER/$RUNFILE"
-SOURCEFILE="/cvmfs/clicdp.cern.ch/software/allpix-squared/3.0.3/x86_64-centos7-gcc12-opt/setup.sh"
+SOURCEFILE="/cvmfs/clicdp.cern.ch/software/allpix-squared/3.0.3/x86_64-el9-gcc12-opt/setup.sh"
 source $SOURCEFILE
 root -b -l $ROOTFILE <<EOF
-.L /cvmfs/clicdp.cern.ch/software/allpix-squared/3.0.3/x86_64-centos7-gcc12-opt/lib/libAllpixObjects.so
+.L /cvmfs/clicdp.cern.ch/software/allpix-squared/3.0.3/x86_64-el9-gcc12-opt/lib/libAllpixObjects.so
 .L processData.C
 writeChargeData(_file0, "dut", "$DATAFILE")
 EOF
